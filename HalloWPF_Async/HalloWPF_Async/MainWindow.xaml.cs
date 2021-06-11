d7b1ff0ffa9279bbfe2296dde9eaf1acaab8e970
André <ruland@outlook.de>
2021-06-11 14:59:50 +0200

    WPF in .net migriert

--- HalloWPF_Async/HalloWPF_Async/MainWindow.xaml.cs
+++ HalloWPF_Async/HalloWPF_Async/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
