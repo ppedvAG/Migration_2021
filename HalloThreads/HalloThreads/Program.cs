5332768daf09523afb9947e902f0139bb67c9500
André <ruland@outlook.de>
2021-06-11 14:12:26 +0200

    Async/Await und Threading

---
+++ HalloThreads/HalloThreads/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HalloThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Threads");
+
+            //Parallel.Invoke(Zähle, Zähle, Zähle);
+
+            Parallel.For(0, 1000000, i => Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} -> {i}"));
+
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+
+        private static void Zähle()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} -> {i}");
+            }
+        }
+    }
+}
---
+++ HalloWPF_Async/HalloWPF_Async/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HalloWPF_Async
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).IsEnabled = false;
+
+            Task.Run(() =>
+            {
+
+                for (int i = 0; i <= 100; i++)
+                {
+                    this.Dispatcher.Invoke(() => pb1.Value = i);
+                    Thread.Sleep(20);
+                }
+                MessageBox.Show("Fertig");
+                this.Dispatcher.Invoke(() => ((Button)sender).IsEnabled = !false);
+            })
+                .ContinueWith(t => MessageBox.Show($"Error{t.Exception.InnerException.Message}"),
+                CancellationToken.None,
+                TaskContinuationOptions.OnlyOnFaulted,
+                TaskScheduler.Default);
+
+        }
+
+        private async void StartAsync(object sender, RoutedEventArgs e)
+        {
+            var conString = "Server=(localdb)\\mssqllocaldb;Database=Northwnd;Trusted_Connection=true;";
+
+            var con = new SqlConnection(conString);
+
+            try
+            {
+                await con.OpenAsync();
+                MessageBox.Show("DB ok");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Schade {ex.Message}");
+            }
+
+        }
+    }
+}
